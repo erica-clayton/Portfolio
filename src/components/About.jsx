48d594578aa21ae640f249cdaf1bd67bce005919
erica-clayton <erica.baertsch@gmail.com>
2023-04-30 15:54:00 -0500

    added in components and styling

---
+++ src/components/About.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div name= "about" className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white">
+        <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-centerw-full h-full">
+            <div className="pb-8">
+                <p className="text-4xl font-bold inline border-b-4 border-gray-500">About</p>
+            </div>
+
+            <p className="text-xl mt-20" >
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti adipisci consequatur, quo eveniet atque similique quia delectus accusantium nobis! Quas pariatur, laudantium odit eos doloribus id velit mollitia assumenda minus asperiores dicta adipisci neque dignissimos eligendi, qui consequatur cumque blanditiis maiores, incidunt quos sint quasi natus? Obcaecati in modi aperiam!
+            </p>
+
+            <br/>
+
+            <p className="text-xl">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem dolorem ipsum labore consequatur eaque, velit accusamus, facilis asperiores officia amet, impedit ea reprehenderit. Optio mollitia explicabo dolore reiciendis commodi voluptatibus aspernatur similique, vitae velit laboriosam quia ex incidunt, fugit nemo voluptatum enim quod nisi, laudantium pariatur quos deleniti. Cum, quibusdam.
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default About
---
+++ src/components/Home.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import HeroImage from "../assets/heroimage.png";
+import {TbArrowRightCircle} from "react-icons/tb"
+
+const Home = () => {
+  return (
+    <div name="home" className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800">
+       <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
+        <div className="flex flex-col justify-center h-full">
+            <h2 className="text-4xl sm:text-7xl font-bold text-white">I am a Full Stack Developer</h2>
+            <p className="text-gray-500 py-4 max-w-md">
+            As a passionate full-stack developer with a talent for quickly
+            adapting to new environments and responsibilities, I am excited to
+            apply my creative and analytical mindset to build innovative
+            solutions. With a background in healthcare as a home health
+            physical therapist assistant, I have developed technical knowledge,
+            problem-solving skills, and strong communication abilities. As a
+            soon-to-be graduate of the NSS, I am dedicated to continuous
+            learning and contributing to a dynamic team.
+            </p>
+
+            <div>
+                <button className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-green-800 to-green-950 cursor-pointer">
+                    Portfolio
+                    <span className="group-hover:rotate-90 duration-300">
+                        <TbArrowRightCircle size={25} className="ml-1"/>
+                    </span>
+                </button>
+            </div>
+        </div>
+
+            <div>
+                <img src={HeroImage} alt="Erica Software Dev" className="rounded-2xl mx-auto w-2/3 md:w-full" />
+            </div>
+       </div>
+    </div>
+  )
+}
+
+export default Home
